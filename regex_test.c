35333ced7fb2b1393a02504dbe0ad909150b1057
Paul Alfille <paul.alfille@gmail.com>
2014-12-23 11:32:52 -0500

    Fancy up the code with more comments, and header message, and a test for extraneous command line arguments, as well as improved menu text. Works under OS X

--- regex_test.c
+++ regex_test.c
@@ -2,12 +2,15 @@
 
 #include <stdio.h> // for getline
 #include <regex.h> // for regex
-#include <stdlib.h> // for exit
 #include <string.h> // for strlen
 
 #define MAXSUB 7
 #define MAXERRORMSG 100
 
+
+// Get a single character for menu choice
+// Need to read rest of line and discard since it's buffered
+// otherwise rest would bleed into text or regex entry
 static char getcharnow( void )
 {
 	static char *  garbage = NULL ;
@@ -17,6 +20,7 @@ static char getcharnow( void )
 	return c ;
 }
 
+// use default regex string if nothing entered
 static char * regexR( char * regex )
 {
 	if ( regex != NULL ) {
@@ -25,6 +29,7 @@ static char * regexR( char * regex )
 	return ".*" ;
 }
 
+// Show default text strings if nothing entered
 static char * regexS( char * string )
 {
 	if ( string != NULL ) {
@@ -33,6 +38,7 @@ static char * regexS( char * string )
 	return "Hello World!" ;
 }
 
+// remove trailing newline from getline strings
 static void trim_newline( char * buffer )
 {
 	if ( buffer != NULL ) {
@@ -48,67 +54,87 @@ static void trim_newline( char * buffer )
 
 int main( int argc, char * argv[] )
 {
-	char * regex = NULL ;
-	size_t regex_len ;
-	char * string = NULL;
-	size_t string_len ;
-	regex_t regc ;
-	int extended = 1 ;
-	int regr = regcomp( &regc, regexR(regex), extended?REG_EXTENDED:0 ) ;
-	do {
-		printf("\n----------------------\n");
-		if ( regr == 0 ) {
-			regmatch_t pmatch[MAXSUB+1] ;
-			
-			int reger = regexec( &regc, regexS(string), MAXSUB, pmatch, 0 ) ;
-			if ( reger != 0 ) {
-				printf("No match\n");
-			} else {
-				int i ;
-				printf("%s\n",regexS(string));
-				printf("0    X    1    X    2    X    3    X    4    X    5    X    6    X    7    X    8    X    \n");
-				for ( i=0 ; i<MAXSUB ; ++i ) {
-					int s = pmatch[i].rm_so ;
-					int e = pmatch[i].rm_eo ;
-					char * b = regexS(string) ;
-					if ( s == -1 ) {
-						break ;
-					}
-					printf("%d:\t%d -> %d  <%.*s>\n",i,s,e,e-s,&b[s]);
-				} 
-			}			
-		} else {
-			char rege[MAXERRORMSG+1] ;
-			regerror( regr, &regc, rege, MAXERRORMSG ) ;
-			printf("XXX Error compiling expression XXX %s\n",rege ) ;
-		}
-		printf("\nChoose:\n") ;	
-		printf( "1. Regex : <%s>\n",regexR(regex) ) ; 
-		printf( "2. String: <%s>\n",regexS(string) ) ;
-		printf( "3. Mode  :  %s \n",extended?"Extended":"Basic" ) ;
-		switch( getcharnow() ) {
-			case 0:
-				exit(0) ;
-			case '1':
-				printf("New regular expression: ");
-				getline( &regex, &regex_len, stdin ) ;
-				trim_newline( regex ) ;
-				if ( regr == 0 ) {
-					regfree( &regc ) ;
-				}
-				regr = regcomp( &regc, regexR(regex), extended?REG_EXTENDED:0 ) ;
-				break ;
-			case '2':
-				printf("New string to test: ");
-				getline( &string, &string_len, stdin ) ;
-				trim_newline( string ) ;
-				break ;
-			case '3':
-				extended = !extended ;
-				break ;
-			default :
-				break ;
-		}		
-	} while (1) ;
-	return 0 ;
+    // regular exression text
+    char * regex = NULL ;
+    size_t regex_len ;
+    
+    // test string text
+    char * string = NULL;
+    size_t string_len ;
+    
+    // compiled string state
+    regex_t regc ;
+    int extended = 1 ;
+    int regr = regcomp( &regc, regexR(regex), extended?REG_EXTENDED:0 ) ;
+    
+    printf("POSIX regular expression tester (%s) -- Paul H Alfille 2014\n",argv[0]) ;
+    printf("\tControl-C to exit at any time\n") ;
+    
+    if ( argc > 1 ) {
+        printf("No command line arguments needed.\n") ;
+    }
+    
+    // infinite loop
+    do {
+        printf("\n----------------------\n");
+        
+        // Show current state
+        if ( regr == 0 ) {
+            // Compiled ok so try it and show matches
+            regmatch_t pmatch[MAXSUB+1] ;
+            
+            int reger = regexec( &regc, regexS(string), MAXSUB, pmatch, 0 ) ;
+            if ( reger != 0 ) {
+                printf("No match\n");
+            } else {
+                int i ;
+                printf("%s\n",regexS(string));
+                printf("0    X    1    X    2    X    3    X    4    X    5    X    6    X    7    X    8    X    \n");
+                for ( i=0 ; i<MAXSUB ; ++i ) {
+                    int s = pmatch[i].rm_so ;
+                    int e = pmatch[i].rm_eo ;
+                    char * b = regexS(string) ;
+                    if ( s == -1 ) {
+                        break ;
+                    }
+                    printf("%d:\t%d -> %d  <%.*s>\n",i,s,e,e-s,&b[s]);
+                }
+            }
+        } else {
+            // Didn't compile so show errors
+            char rege[MAXERRORMSG+1] ;
+            regerror( regr, &regc, rege, MAXERRORMSG ) ;
+            printf("XXX Error compiling expression XXX %s\n",rege ) ;
+        }
+        
+        // Show menu
+        printf("\nChange:\n") ;
+        printf( "1. Regex : <%s>\n",regexR(regex) ) ;
+        printf( "2. String: <%s>\n",regexS(string) ) ;
+        printf( "3. Mode  :  %s \n",extended?"Extended":"Basic" ) ;
+
+        // Get and perform next command
+        switch( getcharnow() ) {
+            case '1':
+                printf("New regular expression: ");
+                getline( &regex, &regex_len, stdin ) ;
+                trim_newline( regex ) ;
+                if ( regr == 0 ) {
+                    regfree( &regc ) ;
+                }
+                regr = regcomp( &regc, regexR(regex), extended?REG_EXTENDED:0 ) ;
+                break ;
+            case '2':
+                printf("New string to test: ");
+                getline( &string, &string_len, stdin ) ;
+                trim_newline( string ) ;
+                break ;
+            case '3':
+                extended = !extended ;
+                break ;
+            default :
+                break ;
+        }		
+    } while (1) ;
+    return 0 ;
 }
